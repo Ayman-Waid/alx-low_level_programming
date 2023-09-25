5ae03bcddc51ab2312e60b0422766019563ea306
Ayman <aymanwaid0@gmail.com>
2023-09-25 10:51:47 -0700

    update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,9 +12,17 @@
 char *_strchr(char *s, char c)
 {
 		int a;
-		for (a = 0 ; s[a] >= '\0' ; a++){
-		if(s[a]==c) return (s+a);
+
+		while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
 		}
-		return NULL;
-		
 }
