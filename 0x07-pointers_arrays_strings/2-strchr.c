2cab04c4e01e2a1bcae7859d73678aa579446ede
Ayman <aymanwaid0@gmail.com>
2023-09-25 10:20:42 -0700

    update

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if(s[i] == c ) return s[i];
+	
+	}
+
+	return (NULL);
+}
